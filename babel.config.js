6f81c1e03a4f43205e4420be045ac8c523183ce4
pawik94 <pawik94@gmail.com>
2026-06-11 12:15:45 +0200

    d

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      [
-        'babel-preset-expo',
-        {
-          unstable_transformProfile: 'hermes-stable',
-        },
-      ],
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
